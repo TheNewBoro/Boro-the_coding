c01681b574ec965bbf60cf778ef0d36aad974885
TheNewBoro <potar1132@gmail.com>
2025-03-20 12:47:06 +0900

    feet new techinc
    first back

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요. 이름을 말해주시겠어요?");
+            string s = Console.ReadLine();
+            Console.WriteLine($"당신의 이름은 {s}군요!");
+            Console.WriteLine("안녕하세요 {0}님!", s);
+        }
+    }
+}
